7bb3e1406be349366158fad92f409477e82aa5d9
Charlie Hammond <charlie.hammond@snowflake.com>
2025-04-15 13:58:19 -0400

    small query change

--- tasty_bytes_dbt_demo/examples/tasty_bytes_example_query.sql
+++ tasty_bytes_dbt_demo/examples/tasty_bytes_example_query.sql
@@ -15,8 +15,8 @@ SELECT
     cl.e_mail,
     SUM(oh.order_total) AS total_sales,
     ARRAY_AGG(DISTINCT oh.location_id) AS visited_location_ids_array
-FROM tb_101.raw_customer.customer_loyalty cl
-JOIN tb_101.raw_pos.order_header oh
+FROM tb_101.raw.customer_loyalty cl
+JOIN tb_101.raw.order_header oh
 ON cl.customer_id = oh.customer_id
 GROUP BY cl.customer_id, cl.city, cl.country, cl.first_name,
 cl.last_name, cl.phone_number, cl.e_mail;
